2e7ca3a051af8913b6b8fb36de0879a44a75f343
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-03-28 18:02:55 +1000

    Update Runner.java

--- src/main/java/com/imjustdoom/Runner.java
+++ src/main/java/com/imjustdoom/Runner.java
@@ -1,9 +1,12 @@
 package com.imjustdoom;
 
+import com.imjustdoom.model.Domain;
 import com.imjustdoom.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class Runner implements CommandLineRunner {
@@ -13,8 +16,24 @@ public class Runner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Timestamp ts = new Timestamp(2014, 1, 1, 0, 0, 0, Timestamp.Time.MONTH);
 
-        new ScanDomain("dl.dropboxusercontent.com", ts, 10000, this.urlService).startScanning();
+        // This is where th indexer starts
+
+        // TODO: Make this configurable
+        String domain = "dl.dropboxusercontent.com";
+
+        // Check if domain exists and if it does load its data
+        Optional<Domain> optionalDomain = this.urlService.getDomain(domain);
+        Timestamp ts;
+        if (optionalDomain.isPresent() && optionalDomain.get().getTimestamp() != null && optionalDomain.get().getTime() != null) {
+            ts = new Timestamp(optionalDomain.get().getTimestamp(), optionalDomain.get().getTime());
+
+        } else {
+            ts = new Timestamp(2014, 1, 1, 0, 0, 0, Timestamp.Time.MONTH);
+
+        }
+
+        // Create the indexer
+        new ScanDomain(domain, ts, 10000, this.urlService).startScanning();
     }
 }
