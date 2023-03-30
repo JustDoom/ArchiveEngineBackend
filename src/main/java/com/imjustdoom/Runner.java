dab9f2d9014c80651b06d38489e43ed554e78598
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-03-31 09:00:19 +1000

    Add basic startup flags to disable scanning and change domain/timestamp start

--- src/main/java/com/imjustdoom/Runner.java
+++ src/main/java/com/imjustdoom/Runner.java
@@ -17,20 +17,34 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        // This is where th indexer starts
+        String domain = "";
+        String timestamp = "20000101000000";
 
-        // TODO: Make this configurable
-        String domain = "dl.dropboxusercontent.com";
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--disable" -> {
+                    System.out.println("Disabling indexer");
+                    return;
+                }
+                case "--domain" -> domain = args[i + 1];
+                case "--timestamp" -> timestamp = args[i +  1];
+            }
+        }
+
+        if (domain.isEmpty()) {
+            System.out.println("Please provide a domain to scan");
+            return;
+        }
+
+        // This is where th indexer starts
 
         // Check if domain exists and if it does load its data
         Optional<Domain> optionalDomain = this.urlService.getDomain(domain);
         Timestamp ts;
         if (optionalDomain.isPresent() && optionalDomain.get().getTimestamp() != null && optionalDomain.get().getTime() != null) {
             ts = new Timestamp(optionalDomain.get().getTimestamp(), optionalDomain.get().getTime());
-
         } else {
-            ts = new Timestamp(2014, 1, 1, 0, 0, 0, Timestamp.Time.MONTH);
-
+            ts = new Timestamp(timestamp, Timestamp.Time.MONTH);
         }
 
         // Create the indexer
